1ddaec818176d579a3623166e61cbe784e5bf9bc
Mayra Cabrera <mcabrera1087@gmail.com>
2016-09-27 16:53:41 -0500

    Updates product on seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 
 puts "Creating products..."
 
-Product.create(name: "USB Cable", price: 100.0)
-Product.create(name: "Prox Box", price: 200.0)
+Product.create(name: "Unicorns", price: 100.0)
+Product.create(name: "Cats", price: 200.0)
+Product.create(name: "Hoodie", price: 200.0)
 Product.create(name: "Mr Robot mask", price: 300.0)
 Product.create(name: "Antena", price: 400.0)
 Product.create(name: "Cool keyboard", price: 500.0)
