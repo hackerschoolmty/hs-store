2eee6ba4131ffeb386108c335b991c12ceb42737
Mayra Cabrera <mcabrera1087@gmail.com>
2016-09-29 12:30:38 -0500

    Add self-relationship to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ActiveRecord::Base
   validates :name, :price, presence: true
   has_many :line_items
   has_many :pictures, as: :picturable
+
+  belongs_to :parent, class_name: "Product"
+  has_many :associated, class_name: "Product", foreign_key: "parent_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929000354) do
+ActiveRecord::Schema.define(version: 20160929021146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160929000354) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "parent_id"
   end
 
   create_table "users", force: :cascade do |t|
