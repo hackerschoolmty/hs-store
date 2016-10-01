63ebb2b8cff7d14c02db0e8c54934b9d5f7f6884
Mayra Cabrera <mcabrera1087@gmail.com>
2016-10-01 14:48:19 -0500

    Add factories & faker for product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 describe Product do
   it "should be valid with name and price" do
-    product = Product.new(name: "cats", price: 100.0)
+    product = FactoryGirl.build(:product)
     expect(product).to be_valid
   end
 
   it "should be invalid withouth a name" do
-    product = Product.new(name: nil, price: 100)
+    product = FactoryGirl.build(:product, name: nil)
     expect(product).to_not be_valid
   end
 
   it "should be invalid without a price" do
-    product = Product.new(name: "cats", price: nil)
+    product = FactoryGirl.build(:product, price: nil)
     expect(product).to_not be_valid
   end
 
   it "should be invalid with a duplicated name" do
-    Product.create(name: "cats", price: 100)
-    product = Product.new(name: "cats", price: 100)
+    FactoryGirl.create(:product)
+    product = FactoryGirl.build(:product)
     expect(product).to_not be_valid
     expect(product).to have(1).error_on(:name)
   end
