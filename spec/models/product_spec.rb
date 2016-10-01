57f5aedf73d9a73dfcea2e16681dcd5b8136b8b7
Mayra Cabrera <mcabrera1087@gmail.com>
2016-10-01 14:50:21 -0500

    add factory girl short syntax

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 describe Product do
   it "should be valid with name and price" do
-    product = FactoryGirl.build(:product)
+    product = build(:product)
     expect(product).to be_valid
   end
 
   it "should be invalid withouth a name" do
-    product = FactoryGirl.build(:product, name: nil)
+    product = build(:product, name: nil)
     expect(product).to_not be_valid
   end
 
   it "should be invalid without a price" do
-    product = FactoryGirl.build(:product, price: nil)
+    product = build(:product, price: nil)
     expect(product).to_not be_valid
   end
 
   it "should be invalid with a duplicated name" do
-    FactoryGirl.create(:product)
-    product = FactoryGirl.build(:product)
+    create(:product)
+    product = build(:product)
     expect(product).to_not be_valid
     expect(product).to have(1).error_on(:name)
   end
